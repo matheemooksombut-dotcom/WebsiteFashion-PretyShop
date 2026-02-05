ecbb33d2ff118a2aede3bd2af9939342b529ccc5
matheemooksombut-dotcom <matheemooksombut@gmail.com>
2026-02-05 22:45:59 +0700

    fontscript

--- scriptimdex.js
+++ scriptimdex.js
@@ -3,28 +3,32 @@ document.addEventListener('DOMContentLoaded', () => {
   const modal = document.querySelector('.login-modal');
   const overlay = document.querySelector('.overlay');
   const closeBtn = document.querySelector('.close');
+  const btnbuy = document.querySelector('.btn-buy');
 
-  if (!userIcon || !modal || !overlay) {
+
+  if (!userIcon || !modal || !overlay || !btnbuy) {
     console.warn('Login elements not found');
     return;
   }
 
-  userIcon.addEventListener('click', () => {
-    console.log('user clicked');
+ function openModal() {
     modal.classList.add('active');
     overlay.classList.add('active');
-  });
-
-  overlay.addEventListener('click', closeModal);
-  closeBtn.addEventListener('click', closeModal);
+  }
 
   function closeModal() {
     modal.classList.remove('active');
     overlay.classList.remove('active');
   }
 
-  
-});
+  // กด icon user
+  userIcon.addEventListener('click', openModal);
 
+  //  กดปุ่ม Buy Now
+  btnbuy.addEventListener('click', openModal);
+
+  overlay.addEventListener('click', closeModal);
+  closeBtn.addEventListener('click', closeModal);
+});
 
 
